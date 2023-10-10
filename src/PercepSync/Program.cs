f53540f78649de21aba18eb357bd6762143fa70d
Peter Yu <2057325+yukw777@users.noreply.github.com>
2023-10-10 11:48:04 -0400

    Update documentation on cross-platform support

--- src/PercepSync/Program.cs
+++ src/PercepSync/Program.cs
@@ -72,19 +72,19 @@
             var localCommand = new Command("local", description: "Use local devices");
             var localCameraDeviceIDOption = new Option<string>(
                 name: "--camera-device-id",
-                description: "Camera device ID",
+                description: "Camera device ID. Only used for Linux.",
                 getDefaultValue: () => LocalConfig.DefaultCameraDeviceId
             );
             localCommand.Add(localCameraDeviceIDOption);
             var localAudioInputDeviceNameOption = new Option<string>(
                 name: "--audio-input-device-name",
-                description: "Audio input device name",
+                description: "Audio input device name. Only used for Linux.",
                 getDefaultValue: () => LocalConfig.DefaultAudioInputDeviceName
             );
             localCommand.Add(localAudioInputDeviceNameOption);
             var localAudioOutputDeviceNameOption = new Option<string>(
                 name: "--audio-output-device-name",
-                description: "Audio output device name",
+                description: "Audio output device name. Only used for Linux.",
                 getDefaultValue: () => LocalConfig.DefaultAudioOutputDeviceName
             );
             localCommand.Add(localAudioOutputDeviceNameOption);
