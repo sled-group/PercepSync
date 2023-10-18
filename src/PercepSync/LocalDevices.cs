f193efd78138a42c95621115f85fe147f331406a
Peter Yu <2057325+yukw777@users.noreply.github.com>
2023-10-18 15:08:42 -0400

    Use \psi native serialization formats for capture devices
    Use Shared<Image> and AudioBuffer to stream data from capture devices,
    e.g., HoloLens 2 and Local.

--- src/PercepSync/LocalDevices.cs
+++ src/PercepSync/LocalDevices.cs
@@ -6,22 +6,24 @@ namespace Sled.PercepSync
     using Microsoft.Psi.Imaging;
     using Microsoft.Psi.Audio;
     using Microsoft.Psi.Interop.Transport;
-    using Microsoft.Psi.Interop.Format;
+    using HoloLensCaptureInterop;
 
     public class LocalDevicesCapture : IDisposable
     {
+        public static readonly string WebcamAddress = "inproc://local-devices-webcam";
         public static readonly string WebcamTopic = "webcam";
+        public static readonly string AudioAddress = "inproc://local-devices-audio";
         public static readonly string AudioTopic = "audio";
         private readonly RendezvousClient rdzvClient;
         private readonly Pipeline pipeline;
-        private readonly NetMQWriter mqWriter;
+        private readonly NetMQWriter<Shared<Image>> mqWebcamWriter;
+        private readonly NetMQWriter<AudioBuffer> mqAudioWriter;
 
         public LocalDevicesCapture(
             string serverAddress,
             int serverPort,
             string cameraDeviceID,
-            string audioDeviceName,
-            string mqAddress = "inproc://local-devices-capture"
+            string audioDeviceName
         )
         {
             pipeline = Pipeline.Create();
@@ -29,36 +31,15 @@ namespace Sled.PercepSync
 
             // Create the webcam component
 #if NET7_0
-            var webcam = new MediaCapture(pipeline, 640, 480, cameraDeviceID, PixelFormatId.YUYV);
-            var serializedWebcam = webcam.Select(
-                (image) =>
-                {
-                    var rgb24Image = image.Resource.Convert(PixelFormat.RGB_24bpp);
-                    var pixelData = new byte[rgb24Image.Size];
-                    rgb24Image.CopyTo(pixelData);
-                    return new RawPixelImage(
-                        pixelData,
-                        rgb24Image.Width,
-                        rgb24Image.Height,
-                        rgb24Image.Stride
-                    );
-                }
-            );
+            var webcam = new MediaCapture(
+                pipeline,
+                640,
+                480,
+                cameraDeviceID,
+                PixelFormatId.YUYV
+            ).Select((image) => Shared.Create(image.Resource.Convert(PixelFormat.RGB_24bpp)));
 #else
-            var webcam = new MediaCapture(pipeline, 640, 480);
-            var serializedWebcam = webcam.Select(
-                (image) =>
-                {
-                    var pixelData = new byte[image.Resource.Size];
-                    image.Resource.CopyTo(pixelData);
-                    return new RawPixelImage(
-                        pixelData,
-                        image.Resource.Width,
-                        image.Resource.Height,
-                        image.Resource.Stride
-                    );
-                }
-            );
+            var webcam = new MediaCapture(pipeline, 640, 480).Out;
 #endif
 
             // Create the audio capture component
@@ -70,25 +51,30 @@ namespace Sled.PercepSync
                     DeviceName = audioDeviceName,
                     Format = WaveFormat.Create16kHz1Channel16BitPcm()
                 }
-            );
+            ).Out;
 #else
-            var audio = new AudioCapture(pipeline, WaveFormat.Create16kHz1Channel16BitPcm());
+            var audio = new AudioCapture(pipeline, WaveFormat.Create16kHz1Channel16BitPcm()).Out;
 #endif
-            var serializedAudio = audio.Select((buffer) => new Audio(buffer.Data));
-
             // NOTE: We can't use RemoteExporter here b/c \psi uses named memory mapped files
             // to serialize complex types, e.g., RawPixelImage, but named memory mapped files
             // are not supported on *nix systems.
             // https://github.com/dotnet/runtime/issues/21863
-            mqWriter = new NetMQWriter(pipeline, mqAddress, MessagePackFormat.Instance);
-            serializedWebcam.PipeTo(
-                mqWriter.AddTopic<RawPixelImage>(WebcamTopic),
-                deliveryPolicy: DeliveryPolicy.LatestMessage
+            mqWebcamWriter = new NetMQWriter<Shared<Image>>(
+                pipeline,
+                WebcamTopic,
+                WebcamAddress,
+                Serializers.SharedImageFormat(),
+                name: nameof(mqWebcamWriter)
             );
-            serializedAudio.PipeTo(
-                mqWriter.AddTopic<Audio>(AudioTopic),
-                deliveryPolicy: DeliveryPolicy.LatestMessage
+            webcam.PipeTo(mqWebcamWriter, deliveryPolicy: DeliveryPolicy.LatestMessage);
+            mqAudioWriter = new NetMQWriter<AudioBuffer>(
+                pipeline,
+                AudioTopic,
+                AudioAddress,
+                Serializers.AudioBufferFormat(),
+                name: nameof(mqAudioWriter)
             );
+            audio.PipeTo(mqAudioWriter, deliveryPolicy: DeliveryPolicy.LatestMessage);
         }
 
         public void Start()
@@ -101,7 +87,11 @@ namespace Sled.PercepSync
                 rdzvClient.Rendezvous.TryAddProcess(
                     new Rendezvous.Process(
                         nameof(LocalDevicesCapture),
-                        new[] { mqWriter.ToRendezvousEndpoint() }
+                        new[]
+                        {
+                            mqWebcamWriter.ToRendezvousEndpoint(),
+                            mqAudioWriter.ToRendezvousEndpoint()
+                        }
                     )
                 );
                 pipeline.RunAsync();
